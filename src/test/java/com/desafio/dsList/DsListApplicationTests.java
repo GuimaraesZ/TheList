917416dce33dff03f90b12ecd3d9b20e6ab5aeb3
Felipe Guimarães <feliphe133bolado@gmail.com>
2025-05-25 16:08:37 -0300

    Configurações iniciais do projeto, estrutura de diretórios, arquivos de propriedades e setup do Maven

---
+++ src/test/java/com/desafio/dsList/DsListApplicationTests.java
@@ -0,0 +1,15 @@
+package com.desafio.dsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
